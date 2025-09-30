857c5519268c879113947fe1a118af5e661cf8f9
MiguelDV84 <miguel.diazv84@gmail.com>
2025-09-30 10:25:33 +0200

    30/09/2025 AD

---
+++ AD/Actividad2_JBDC/src/main/java/org/mdv/conexion/Conexion.java
@@ -0,0 +1,10 @@
+package org.mdv.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Conexion {
+
+}
